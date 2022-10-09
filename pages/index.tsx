150113bd340324b941f990d81715316a847db066
jinseok9338 <jinseok9338@gmail.com>
2022-10-09 11:40:35 +0000

    minor update

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,7 @@ export default function HomePage() {
       <div className={styles.container}>
         <div className={styles.background}>
           <h1>I BUILD</h1>
-          <h1>WEBSITES</h1>
+          <h1>SOMETHING</h1>
         </div>
         <div className={styles.foreground}>
           <div className={styles.content}>
