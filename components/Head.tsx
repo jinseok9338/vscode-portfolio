3904dd677dc218ad79841fe71c3452b91343b8e5
jinseok9338 <jinseok9338@gmail.com>
2022-10-10 11:46:48 +0000

    update header

--- components/Head.tsx
+++ components/Head.tsx
@@ -1,4 +1,4 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const CustomHead = ({ title }) => {
   return (
@@ -6,19 +6,14 @@ const CustomHead = ({ title }) => {
       <title>{title}</title>
       <meta
         name="description"
-        content="Nitin Ranganath is an avid full stack web developer building websites and applications you'd love to use"
+        content="Jinseok Seo is an avid full stack web developer building websites and applications you'd love to use"
       />
+
+      <meta property="og:title" content="Jinseok Seo's Portfolio" />
       <meta
-        name="keywords"
-        content="nitin ranganath, nitin, ranganath, web developer portfolio, nitin web developer, nitin developer, mern stack, nitin ranganath portfolio, vscode-portfolio"
+        property="og:url"
+        content="https://vscode-portfolio-six.vercel.app/github"
       />
-      <meta property="og:title" content="Nitin Ranganath's Portfolio" />
-      <meta
-        property="og:description"
-        content="A full-stack developer building websites that you'd like to use."
-      />
-      <meta property="og:image" content="https://imgur.com/4zi5KkQ.png" />
-      <meta property="og:url" content="https://vscode-portfolio.vercel.app" />
       <meta name="twitter:card" content="summary_large_image" />
     </Head>
   );
@@ -27,5 +22,5 @@ const CustomHead = ({ title }) => {
 export default CustomHead;
 
 CustomHead.defaultProps = {
-  title: 'Nitin Ranganath',
+  title: "Jinseok Seo",
 };
